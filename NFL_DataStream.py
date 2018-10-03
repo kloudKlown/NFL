0e60c1ab520ae5d2f4f20bd73f6bf11cf977d7fc
kloudKlown <suhas.servesh@gmail.com>
2018-10-02 20:34:57 -0400

    Cleanup to make data easily inputable into SQL

--- NFL_DataStream.py
+++ NFL_DataStream.py
@@ -7,30 +7,33 @@ from bs4 import BeautifulSoup
 import inflect 
 
 
-YEAR = 2015 
+YEAR = [2015,2016,2017,2018] 
 positionHeaders = {}
 
 
-
-def ClearSpecialCharacters(data):
+def ClearSpecialCharacters(data, header):
+    if header:
+        data = data.replace('%', 'per')
     data = re.sub('[^a-zA-Z0-9 \n\.\s]', '', data)
+    data = data.replace(' ', '')
     return data
 
 
 # Extract team urls and names
 def ExtractTeams():
-    teamData = subprocess.check_output(['curl', 'https://www.pro-football-reference.com/teams/'], shell = True)
-    soup = BeautifulSoup(teamData[10000:], features='html.parser')
-    teamDiv = soup.find('div', {'id': 'div_teams_active' })
-    teams =  teamDiv.findAll('a')
-    for each in teams:
-        if re.match('.*<a href="/teams/.*', str(each)):	
-            print(each['href'])
-            ExtractPlayersFromRoster(each['href'], each.text)
+    for year in YEAR:
+        teamData = subprocess.check_output(['curl', 'https://www.pro-football-reference.com/teams/'], shell = True)
+        soup = BeautifulSoup(teamData[10000:], features='html.parser')
+        teamDiv = soup.find('div', {'id': 'div_teams_active' })
+        teams =  teamDiv.findAll('a')
+        for each in teams:
+            if re.match('.*<a href="/teams/.*', str(each)):	
+                print(each['href'])
+                ExtractPlayersFromRoster(each['href'], each.text, year)
 
 # Extract Players from team Roster
-def ExtractPlayersFromRoster(teamURL, teamName):
-    teamURL = 'https://www.pro-football-reference.com' + teamURL + str(YEAR) + '_roster.htm' 
+def ExtractPlayersFromRoster(teamURL, teamName, year):
+    teamURL = 'https://www.pro-football-reference.com' + teamURL + str(year) + '_roster.htm' 
     print(teamURL)
     teamRoster = subprocess.check_output(['curl' , teamURL], shell = True)
     soup = BeautifulSoup(teamRoster[10000:], features='html.parser')
@@ -54,12 +57,14 @@ def ExtractPlayersFromRoster(teamURL, teamName):
             
 
         if len(position) > 0 and len(player) > 0:
-            player = ClearSpecialCharacters(player)
-            ExtractPlayersData(player, position, playerLink)
+            player = ClearSpecialCharacters(player, False)
+            ExtractPlayersData(player, position, playerLink, year)
 
-def ExtractPlayersData(player, playerPosition, playerLink):
+
+
+def ExtractPlayersData(player, playerPosition, playerLink, year):
     playerLink = playerLink.split('.')[0]
-    playerGameLog = 'https://www.pro-football-reference.com' + playerLink + '/gamelog/2015/'
+    playerGameLog = 'https://www.pro-football-reference.com' + playerLink + '/gamelog/' + str(year) +'/'
     playerGameLog = subprocess.check_output(['curl' , playerGameLog], shell = True)
     soup = BeautifulSoup(playerGameLog[10000:], features='html.parser')
     playerGameLogData = soup.find('div', {'id': 'div_stats'}) 
@@ -78,7 +83,7 @@ def ExtractPlayersData(player, playerPosition, playerLink):
         for eachHeader in (playerGameLogDataHeader.findAll('tr')[0]).findAll('th'):
                 headerName = ""
                 if (len(str(eachHeader.text)) > 0):
-                    headerName = eachHeader.text
+                    headerName = ClearSpecialCharacters(eachHeader.text, True)
                     
                 else:
                     length = p.number_to_words(len(header) + 1)
@@ -102,7 +107,7 @@ def ExtractPlayersData(player, playerPosition, playerLink):
                     headerName = length
 
                 ### Clear Special charcters
-                headerName = ClearSpecialCharacters(headerName)
+                headerName = ClearSpecialCharacters(headerName, True)
                 headerMain.append(headerName) 
 
     # Find key from headerlist to append to main headers                
@@ -116,7 +121,7 @@ def ExtractPlayersData(player, playerPosition, playerLink):
 
     # Combine headers
     for i in range(0, len(headerMain)):
-        headerMain[i] = str((FindKey(header, i))) + headerMain[i] + ','
+        headerMain[i] = str((FindKey(header, i))) + headerMain[i]
     
     headerMain.insert(0, "PlayerPosition")
     headerMain.insert(0, "PlayerName")
@@ -126,17 +131,20 @@ def ExtractPlayersData(player, playerPosition, playerLink):
     fileToWrite = open('tableInsert.csv', 'a+')
     InsertIntoTable =  'INSERT INTO NFLTABLEDATA ('  + str(','.join(headerMain)) + ') VALUES \n'
     fileToWrite.write(InsertIntoTable) 
-        
+    count = 0        
     # Body of the Gamelog
     playerGameLogDataBody = playerGameLogData.find('tbody')
     for eachTD in playerGameLogDataBody.findAll('tr'):
-        playerData =  '\'' + player + '\''  + ',' + '\'' +  playerPosition + '\'' +  ','
-        count = 0
+        if count == 0:
+            playerData =  '(\'' + player + '\''  + ',' + '\'' +  playerPosition + '\'' +  ','
+        else:
+            playerData =  ',(\'' + player + '\''  + ',' + '\'' +  playerPosition + '\'' +  ','        
         playerData = playerData + '\'' + str(count) + '\'' 
         for each in eachTD.findAll('td'):
-            playerData = playerData + ',' + '\'' + ClearSpecialCharacters(each.text) + '\''
+            playerData = playerData + ',' + '\'' + ClearSpecialCharacters(each.text, False) + '\''
 
-        playerData = '(' + playerData + '), \n'
+        playerData = playerData + ') \n'        
+        # input(playerData)
         fileToWrite.write(playerData)
 
         count += 1
